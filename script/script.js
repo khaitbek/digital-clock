122ae2f794e75b6e6d58c1783ac3878d89d0ff7e
khaitbek <khaitbek2005@gmail.com>
2022-07-29 22:44:46 +0500

    Fix minor bug

--- script/script.js
+++ script/script.js
@@ -2,15 +2,16 @@ let elTime = document.querySelector(".time");
 
 setInterval(()=>{
     let date = new Date();
-    let hours = date.getHours() + 7;
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    if(hours > 12) hours -= 12;
+    let hours = date.getUTCHours();
+    let minutes = date.getUTCMinutes();
+    let seconds = date.getUTCSeconds();
+    let day_night = "AM"
+    if(hours > 12){
+        day_night = "PM"
+        hours = hours - 12
+    }
     if(hours < 10) hours = hours;
-    let text = hours <= 12 ? "AM" : "PM";
     if(minutes < 10) minutes = "0" + minutes;
     if(seconds < 10) seconds = "0" + seconds;
-
-    elTime.textContent = `${hours < 10 ? "0" + hours : hours }:${minutes}:${seconds} ${text}`
-
+    elTime.textContent = `${hours < 10 ? "0" + hours : hours }:${minutes}:${seconds} ${day_night}`
 })
