aef1cb7a42d7852295699960f884a89ef5a0bd58
khaitbek <khaitbek2005@gmail.com>
2022-07-31 20:17:37 +0500

    Fix timezone issue

--- script/script.js
+++ script/script.js
@@ -5,13 +5,8 @@ setInterval(()=>{
     let hours = date.getUTCHours();
     let minutes = date.getUTCMinutes();
     let seconds = date.getUTCSeconds();
-    let day_night = "AM"
-    if(hours > 12){
-        day_night = "PM"
-        hours = hours - 12
-    }
     if(hours < 10) hours = hours;
     if(minutes < 10) minutes = "0" + minutes;
     if(seconds < 10) seconds = "0" + seconds;
-    elTime.textContent = `${hours < 10 ? "0" + hours : hours }:${minutes}:${seconds} ${day_night}`
+    elTime.textContent = `${hours < 10 ? "0" + hours : hours }:${minutes}:${seconds}`
 })
